968a1214299bf2a1934df778acca7b7c5c35718b
omerbariskayaalp <omerbariskayaalp@gmail.com>
2023-07-16 16:36:42 +0300

    datatypes

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int a=5;
+        double b=4.1;
+        Boolean reel=true;
+        if (reel==true) {
+            System.out.println("Hello world");
+        }
+        else  {
+            System.out.println("wrong!!!");
+        }
+    }
+}
